8d7767a42faf58a78122044850121a4e386ad2da
PetiSui <minittf@gmail.com>
2024-03-13 19:19:34 +0100

    removed boilerplate

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0DtoCNXffSVUEq2NHqXJkWUDQK/6rBvULc9IHsyLm7E=\"\n}"
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div>Mi APP</div>
+  );
+}
