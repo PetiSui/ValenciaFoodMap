38983a7fda4eed2a82d1ab675307ecf25ed84a96
PetiSui <minittf@gmail.com>
2024-03-23 20:38:12 +0100

    Tags collapsible

--- src/app/components/Tags.jsx
+++ src/app/components/Tags.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Tags = ({ tags }) => {
+  const [expanded, setExpanded] = useState(false);
 
-  if(!tags || tags.length === 0) return;
-  //console.log(tags);
+  if (!tags || tags.length === 0) return;
+  console.log(tags);
 
   let categories = Array.from({ length: tags.length });
 
-  for (let i = 0; i < tags.length; i++) {
+  const baselineLength = tags.length > 2 ? 2 : tags.length;
+  let length = expanded === true ? tags.length : baselineLength;
+
+  for (let i = 0; i < length; i++) {
     categories[i] = (
-      <span key={crypto.randomUUID()} className={["tag", tags[i].toLowerCase()].join(" ")}>
+      <span
+        key={crypto.randomUUID()}
+        className={["tag", tags[i].toLowerCase()].join(" ")}
+      >
         {tags[i]}
       </span>
     );
   }
 
-  return <div className="tags">{categories}</div>;
+  return (
+    <div className="tags">
+      {categories}
+      {tags.length > 2 ? (
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          size="xl"
+          className={`tag_expand ${
+            expanded ? "fa-rotate-180" : "fa-rotate-360"
+          } text-black`}
+          onClick={() => setExpanded((prev) => !prev)}
+        ></FontAwesomeIcon>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default Tags;
