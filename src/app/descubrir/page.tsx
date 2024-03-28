491727db5652537f2a47c443a14bad3a2a3f244c
PetiSui <minittf@gmail.com>
2024-03-28 19:08:01 +0100

    fixx

--- src/app/descubrir/page.tsx
+++ src/app/descubrir/page.tsx
@@ -3,7 +3,11 @@ import { ToastContainer } from "react-toastify";
 
 export default async function App() {
   const getCards = async () => {
-    const res = await fetch("http://localhost:3000/api/cards/", {
+    // const apiUrl = "http://localhost:3000";
+    const apiUrl = process.env.API_URL;
+    // console.log(process.env.API_URL);
+    
+    const res = await fetch(`${apiUrl}/api/cards/`, {
       headers: { "Content-Type": "application/json" },
     });
     return res.json();
