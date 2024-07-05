405c3b041bc5f66e74099806aa00f3d2b25fd499
PetiSui <minittf@gmail.com>
2024-07-05 20:14:49 +0200

    ??

--- src/app/api/map/route.js
+++ src/app/api/map/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(req) {
   const con = await initMongoose();
   console.log("Hit db connect", new Date().getSeconds());
-  const data = await Card.find({}).select("address name lat lng photos categories url").sort({name: 1});
+  const data = await Card.find({}).select("address name lat lng photos categories url").sort({name: 1}) || [];
 
   return new NextResponse(JSON.stringify(data));
 }
--- src/app/components/PopUpCard.tsx
+++ src/app/components/PopUpCard.tsx
@@ -23,7 +23,7 @@ export default function PopUpCard(data:DetailsProps) : JSX.Element {
         <p className="text-xl font-semibold !m-0">{data.name}</p>
         <a href={data?.url} target="_blank" className="text-l font-light !m-0 !mt-2">{data.address}</a>
         <Link
-            className="font-semibold flex-1 bg-lightblack !text-white border-0 rounded px-5 py-3 text-center hover:bg-slate-300 hover:text-lightblack"
+            className="font-semibold flex-1 bg-lightblack !text-white border-0 rounded px-5 py-3 text-center hover:!bg-slate-200 hover:!text-lightblack"
             href={`/descubrir/${data._id}`}
           >
             <FontAwesomeIcon
