5280ca9976f28541015d86fcddde63eae6656d54
PetiSui <minittf@gmail.com>
2024-07-05 22:53:54 +0200

    FIX??

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -4,30 +4,28 @@ export default async function Mapa() {
 
     if (!process.env.NEXT_PUBLIC_BASE_API_URL) return null;
 
-    //const getCards = async () => {
-    //  //const apiUrl = "http://localhost:3000";
-    //  const apiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
-  //
-    //  // console.log(process.env.API_URL);
-  //
-    //  const res = await fetch(
-    //    `${apiUrl}/api/map`,
-    //    {
-    //      headers: {
-    //        cacheStore: "no-store",
-    //        accept: 'application/json'
-    //      },
-    //    }
-    //  );
-  //
-    //  const jsonData = res.json();
-  //
-    //  return jsonData;
-    //};
-  //
-    //const datax = await getCards();
-    const datax: any[] = [];
-
+    const getCards = async () => {
+      //const apiUrl = "http://localhost:3000";
+      const apiUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
+  
+      // console.log(process.env.API_URL);
+  
+      const res = await fetch(
+        `${apiUrl}/api/map`,
+        {
+          headers: {
+            cacheStore: "no-store",
+            accept: 'application/json'
+          },
+        }
+      );
+  
+      const jsonData = res.json();
+  
+      return jsonData;
+    };
+  
+    const datax = await getCards();
 
   return <MapCaller data={datax}></MapCaller>
 }
