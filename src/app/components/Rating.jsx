e2b1b0d9c729640a268a139a3ab1ed5310d0fc64
PetiSui <minittf@gmail.com>
2025-02-21 22:49:41 +0100

    added rating number on cards

--- src/app/components/Rating.jsx
+++ src/app/components/Rating.jsx
@@ -1,45 +1,52 @@
-import React from 'react'
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
 import { faStarHalf as faStarHalfSolid } from "@fortawesome/free-solid-svg-icons";
 
 const Rating = ({ starCount }) => {
-    if (!starCount) return null;
+  if (!starCount) return null;
 
-    let MAX_RATING = 5;
-    let rating = parseFloat(starCount);
-    if (rating > MAX_RATING) {
-      rating = MAX_RATING;
-    }
-    if (rating < 0.0) {
-      rating = 0.0;
-    }
-    let wholeStars = Math.floor(rating);
+  let MAX_RATING = 5;
+  let rating = parseFloat(starCount);
+  if (rating > MAX_RATING) {
+    rating = MAX_RATING;
+  }
+  if (rating < 0.0) {
+    rating = 0.0;
+  }
+  let wholeStars = Math.floor(rating);
 
-    let stars = Array.from({ length: MAX_RATING });
+  let stars = Array.from({ length: MAX_RATING });
 
-    let i;
-    for (i = 0; i < wholeStars; i++) {
-      stars[i] = (
-        <FontAwesomeIcon
-          icon={faStarSolid}
-          className="star"
-          key={i}
-        ></FontAwesomeIcon>
-      );
-    }
+  let i;
+  for (i = 0; i < wholeStars; i++) {
+    stars[i] = (
+      <FontAwesomeIcon
+        icon={faStarSolid}
+        className="star"
+        key={i}
+      ></FontAwesomeIcon>
+    );
+  }
 
-    if (rating - wholeStars > 0 && rating - wholeStars < 1) {
-      stars[i] = (
-        <FontAwesomeIcon
-          icon={faStarHalfSolid}
-          className="star half-star"
-          key={i}
-        ></FontAwesomeIcon>
-      );
-    }
+  if (rating - wholeStars > 0 && rating - wholeStars < 1) {
+    stars[i] = (
+      <FontAwesomeIcon
+        icon={faStarHalfSolid}
+        className="star half-star"
+        key={i}
+      ></FontAwesomeIcon>
+    );
+  }
 
-    return <div className="rating">{stars}</div>;
-  };
+  return (
+    <div className="rating flex flex-col items-start gap-[.15rem]">
+      <p className="text-white drop-shadow-xl drop-shadow-black text-m mr-2 border border-neutral-500/55 bg-neutral-400/55 hover:bg-neutral-400/75 transition-all rounded pt-[.15rem] pb-[.075rem] px-[.15rem]">
+        {rating}
+      </p>
+      <div>{stars}</div>
+    </div>
+  );
+};
 
-export default Rating
+export default Rating;
